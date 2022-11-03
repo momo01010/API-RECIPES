9ff0444e34ffae8df70346c0ba87f430acd83c48
Guillermo Vidal <gcv005@gmail.com>
2022-11-02 22:02:49 -0500

    finishing api-recipes

---
+++ src/recipes/recipes.controllers.js
@@ -0,0 +1,116 @@
+const uuid = require('uuid')
+const Recipes = require('../models/recipes.models')
+const Users = require('../models/users.models')
+const Categories = require('../models/categories.models')
+const Instructions = require('../models/instructions.models')
+const RecipeIngredients = require('../models/recipes_ingredients.models')
+const Ingredients = require('../models/ingredients.models')
+const Types = require('../models/types.models')
+
+
+const getAllRecipes = async () => {
+    const data = await Recipes.findAll({
+        attributes:{
+            exclude: ['userId', 'categoryId', 'createdAt', 'updatedAt']
+        },
+        include: [
+            {
+                model: Categories
+            },
+            {
+                model: Users
+            },
+            {
+                model: Instructions,
+                attributes: ['id', 'step', 'description']
+            },
+            {
+                model: RecipeIngredients,
+                include: {
+                    model: Ingredients,
+                    include: {
+                        model: Types
+                    }
+                }
+            }
+        ]
+    })
+    return data
+}
+
+const getRecipeById = async (id) => {
+    const data = await Recipes.findOne({
+        where: {
+            id
+        },
+        attributes: {
+            exclude: ['userId', 'categoryId', 'createdAt', 'updatedAt']
+        },
+        include: [
+            {
+                model: Categories
+            },
+            {
+                model: Users,
+                attributes: ['id', 'firstName', 'lastName']
+            },
+            {
+                model: Instructions,
+                attributes: ['step', 'description']
+            },
+            {
+                model: RecipeIngredients,
+                include: {
+                    model: Ingredients,
+                    include: {
+                        model: Types
+                    }
+                }
+            }
+        ]
+    })
+    return data
+}
+
+const createRecipe = async (data) => {
+    const response = await Recipes.create({
+        id: uuid.v4(),
+        title: data.title,
+        description: data.description,
+        urlImg: data.urlImg,
+        time: data.time,
+        portions: data.portions,
+        userId: data.userId,
+        categoryId: data.categoryId,
+        origin: data.origin,
+        likes: data.likes
+    })
+    return response
+}
+
+const updateRecipe = async (id, data) => {
+    const response = await Recipes.update(data, {
+        where: {
+            id
+        }
+    })
+    return response
+}
+
+const deleteRecipe = async (id) => {
+    const data = await Recipes.destroy({
+        where: {
+            id
+        }
+    })
+    return data
+}
+
+module.exports = {
+    getAllRecipes,
+    getRecipeById,
+    createRecipe,
+    updateRecipe,
+    deleteRecipe
+}
+
